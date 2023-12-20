fd5fd3739e4a2e677bf5775dbb8b40d16c35eafa
Dmitriy Monakhov <dimamon6032403@gmail.com>
2023-12-20 18:35:15 +0100

    CacheManager implemented

--- YtDlpViewsDumper.py
+++ YtDlpViewsDumper.py
@@ -1,4 +1,5 @@
 import argparse
+from genericpath import isdir
 import subprocess
 import json
 from unittest import skip
@@ -14,6 +15,7 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor
 import atexit
 import logging
+import shutil
 
 
 LOG_FORMAT = "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"
@@ -35,6 +37,89 @@ ytdl_format_options = {
 
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 
+class CacheManager:
+    def __init__(self, cache_dir_global, chunk_size=32):
+        self.cache_dir_global = cache_dir_global
+        self.cache_buffer = {}   
+        self.chunk_size = chunk_size
+
+    def get_cache_dir_local(self, cache_name):
+        cache_dir_local = os.path.join(self.cache_dir_global, cache_name)
+        return cache_dir_local
+    
+    def clear_cache(self, cache_name):
+        path = self.get_cache_dir_local(cache_name)
+        shutil.rmtree(path)
+    
+    def append(self, cache_name, records: dict | list[dict], chunk_size_override = None):
+        if chunk_size_override:
+            chunk_size_local = chunk_size_override
+        else:
+            chunk_size_local = self.chunk_size
+
+        if not isinstance(records, list):
+            records = [records]
+            
+        cache_dir = self.get_cache_dir_local(cache_name)
+        if not os.path.isdir(cache_dir):
+            os.makedirs(cache_dir)
+            logger.debug(f"Created cache dir for {cache_name}")
+        
+        if not self.cache_buffer[cache_name]:
+            self.cache_buffer[cache_name] = []
+
+        for record in records:
+            if len(self.cache_buffer[cache_name]) > chunk_size_local:
+                self.flush_buffers()
+                
+            self.cache_buffer[cache_name].append(record)
+    
+    def read_cache(self, cache_name) -> list[dict]:
+        cache_entries = []        
+
+        cache_dir = self.get_cache_dir_local(cache_name)
+        if not os.path.isdir(cache_dir):
+            logger.debug(f"Cache {cache_name} does not exist")
+            return None
+
+        for file_path_relative in os.listdir(cache_dir):
+            file_path = os.path.join(cache_dir, file_path_relative)
+            if "cache.json" not in file_path_relative:
+                logger.debug(f"Unexpected file {file_path} in cache {cache_name}")
+                continue
+            
+            with open(file_path, "r") as infile:
+                cache_entries += json.load(infile)
+
+        return cache_entries
+    
+    def flush_buffers(self):
+        for cache_name, buffer in self.cache_buffer.items():
+            if not buffer:
+                continue
+            
+            logger.debug(f"Flushing {len(buffer)} buffered records for cache {cache_name}")            
+            cache_dir = self.get_cache_dir_local(cache_name)
+            chunk_names = os.listdir(cache_dir)
+            chunk_max = 0
+            for chunk_name in chunk_names:
+                chunk_num_str = chunk_name[len(cache_name) + 1:len(".cache.json")]
+                chunk_num = int(chunk_num_str)
+                if chunk_max < chunk_num:
+                    chunk_max = chunk_num
+                    
+            logger.debug(f"Max existing chunk number for {cache_name}: {chunk_max}")
+            chunk_num_current = chunk_max + 1
+            cache_chunk_name = os.path.join(cache_dir, f"{cache_name}-{chunk_num_current:6}.cache.json")
+            
+            with open(cache_chunk_name) as fout:
+                json.dump(buffer, fout, indent=4)
+            
+            logger.debug(f"Flushed chunk {chunk_num_current} with {len(buffer)} records to {cache_chunk_name}")
+
+            buffer.clear()
+
+cache_manager = None    
 
 def get_video_metadata(video, credentials = None, yt_dlp_path = "yt-dlp"):
     
@@ -579,6 +664,7 @@ async def main():
 
     cache_dir = args.cache_dir
     cache_expiration_seconds = args.cache_expiration * 24 * 60 * 60
+    cache_manager = CacheManager(cache_dir)
 
     date_from_str = args.date_from
     if date_from_str:
