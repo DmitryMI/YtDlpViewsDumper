54ac2e5a41d7572b9184537d41839dcc41ff326a
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-05-30 13:13:09 +0200

    Milestone added

--- YtDlpViewsDumper.bat
+++ YtDlpViewsDumper.bat
@@ -8,6 +8,6 @@ set python_exe=python
 
 SET PYTHONPATH=%PYTHONPATH%;grabbers
 
-%python_exe% YtDlpViewsDumper.py --channel "%url%" --milestone_file "WarInUkraineMilestones.txt" --vk_api_json vk_api.json --ma_degree 10 --title_regex ".*КСТАТИ.*"
+%python_exe% YtDlpViewsDumper.py --channel "%url%" --milestone_file "WarInUkraineMilestones.txt" --vk_api_json vk_api.json --ma_degree 10
 
 pause
